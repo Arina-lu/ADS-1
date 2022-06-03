934432cd60ff82751bad999748e7af152c532c4c
Arina-lu <99293252+Arina-lu@users.noreply.github.com>
2022-06-03 20:50:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,32 +4,32 @@
 
 
 bool checkPrime(uint64_t value) {
- for (uint64_t i = 2; i < value / 2 + 1; i++) {
+for (uint64_t i = 2; i < value / 2 + 1; i++) {
         if (value % i == 0) { return false; }
     }
-	return true;
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-uint64_t p = 0;
+    uint64_t pr = 0;
     for (uint64_t i = 2; i <= n; i++) {
-        if (checkPrime(i)) { p++; }
+        if (checkPrime(i)) { pr++; }
     }
-    return p;
+    return pr;
 }
 
 uint64_t nextPrime(uint64_t value) {
-value++;
+    value++;
     while (true) {
         if (checkPrime(value)) { return value; }
         value++;
-    }  
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
- uint64_t su = 0;
+    uint64_t s = 0;
     for (uint64_t i = 2; i < hbound; i++) {
-        if (checkPrime(i)) { su += i; }
+        if (checkPrime(i)) { s += i; }
     }
-    return su;
+    return s;
 }
