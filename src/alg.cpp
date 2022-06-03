c1413aa68bde497254ff1fe87f272b40d381108c
Arina-lu <99293252+Arina-lu@users.noreply.github.com>
2022-06-03 21:14:14 +0300

    Create alg.cpp

---
+++ src/alg.cpp
@@ -0,0 +1,34 @@
+#include <cstdint>
+#include "alg.h"
+
+
+bool checkPrime(uint64_t value) {
+for (uint64_t i = 2; i < value / 2 + 1; i++) {
+        if (value % i == 0) { return false; }
+    }
+    return true;
+}
+
+uint64_t nPrime(uint64_t n) {
+    uint64_t pr = 0;
+    for (uint64_t i = 2; i <= n; i++) {
+        if (checkPrime(i)) { pr++; }
+    }
+    return pr;
+}
+
+uint64_t nextPrime(uint64_t value) {
+    value++;
+    while (true) {
+        if (checkPrime(value)) { return value; }
+        value++;
+    }
+}
+
+uint64_t sumPrime(uint64_t hbound) {
+    uint64_t s = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) { s += i; }
+    }
+    return s;
+}
